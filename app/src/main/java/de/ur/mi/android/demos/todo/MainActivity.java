9d57b45f33015e291c7199e4d78b7a7aa9450b63
Alexander Bazo <alexanderbazo@googlemail.com>
2021-05-13 09:43:12 +0200

    Add comments

--- app/src/main/java/de/ur/mi/android/demos/todo/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/demos/todo/MainActivity.java
@@ -15,10 +15,28 @@ import java.util.Collections;
 import de.ur.mi.android.demos.todo.tasks.Task;
 import de.ur.mi.android.demos.todo.ui.TaskListAdapter;
 
+
+/**
+ * ToDo-Liste
+ * <p>
+ * Diese App erlaubt den Nutzer*innen das Zusammenstellen einer einfachen Aufgabenliste. Dazu können
+ * neue Aufgaben erstellt und der Zustand (offen/geschlossen) existierende Aufgaben umgeschaltet werden.
+ * In dieser Version der App werden die Einträge der Liste nicht gespeichert, d.h. nach Beenden der
+ * App gehen alle erstellten Aufgaben verloren. Diese Variante stellt eine einfache Lösung für die
+ * ursprüngliche Aufgabenstellung dar.
+ * <p>
+ * Diese Activity initialisiert die notwendigen Komponenten des User Interface (Eingabefelder und
+ * ListView), fängt Eingabe-Events auf den Elementen und verarbeitet diese. In der Activity wird
+ * auch die ArrayList mit Task-Objekten verwaltet, die den inhaltlichen Kern der Anwendung ausmacht.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
+    // Liste mit den aktuelle in der App verwalteten Aufgaben
     private ArrayList<Task> tasks;
+    // Adapter zur Verbindung der Datenstruktur (tasks) mit dem ListView, der die Inhalte im UI anzeigt
     private TaskListAdapter taskListAdapter;
+    // Eingabefeld für den Beschreibungstext neuer Aufgaben
     private EditText taskDescriptionInput;
 
     @Override
@@ -28,49 +46,82 @@ public class MainActivity extends AppCompatActivity {
         initUI();
     }
 
+    /**
+     * Initialisiert eine neue ArrayList zum Speichern der Aufgaben
+     */
     private void initTasks() {
         tasks = new ArrayList<>();
     }
 
+    /**
+     * Initialisiert (in weiteren Methoden) das User Interface der App
+     */
     private void initUI() {
         setContentView(R.layout.activity_main);
         initListView();
         initInputElements();
     }
 
+    /**
+     * Initialisiert Adapter und Liste sowie die Interaktionsmöglichkeiten auf der Liste
+     */
     private void initListView() {
+        // Hier erstellen wir eine Instanz unseres angepassten ArrayAdapters
         taskListAdapter = new TaskListAdapter(this);
         ListView taskList = findViewById(R.id.task_list);
+        // ListView und Adapter werden verbunden
         taskList.setAdapter(taskListAdapter);
+        // Hier registrieren wir einen Listener, der informiert wird, wenn Einträge des ListViews länger angeklickt werden
         taskList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            // Diese Callback-Funktion wird immer aufgerufen, wenn ein Listeneintrag länger angeklickt wird
+            // Über den Parameter position erhalten wir die Information, welche Element (Position innerhalb des ListViews) angeklickt wurde
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // Die Verarbeitung des Events (Umschalten des Zustands des angeklickten "Tasks") erfolgt in einer separaten Methode
                 toggleTaskAtPosition(position);
                 return true;
             }
         });
     }
 
+    /**
+     * Initialisiert die Eingabeelemente und die Interaktionsmöglichkeiten auf diesen
+     */
     private void initInputElements() {
         taskDescriptionInput = findViewById(R.id.input_text);
         Button inputButton = findViewById(R.id.input_button);
         inputButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Wenn der Button zum Hinzufügen einer neuen Aufgabe angeklickt wird, lesen wir den aktuellen Inhalt des Textfelds aus ...
                 String currentInput = taskDescriptionInput.getText().toString();
+                // ... und versuchen, aus diesem Inhalt einen neuen Task zu erstellen
                 onUserInputClicked(currentInput);
             }
         });
     }
 
+    /**
+     * Fügt einen neuen Task zur ArrayList hinzu und aktualisiert im Anschluss die Inhalte des Adapters
+     *
+     * @param description Beschreibung des neuen Tasks
+     */
     private void addTask(String description) {
         Task taskToAdd = new Task(description);
         tasks.add(taskToAdd);
+        // Nach der Änderung an der Datenbasis wird der Adapter aktualisiert
         updateTasksInAdapter();
     }
 
+    /**
+     * Schaltet den Zustand des Tasks an der angegebene Position der List um: Ist der Task aktuell offen, wird
+     * er nach dem Aufruf dieser Methode als geschlossen markiert sein
+     *
+     * @param position Listenposition des Tasks, dessen Zustand geändert werden soll
+     */
     private void toggleTaskAtPosition(int position) {
         Task taskToToggle = tasks.get(position);
+        // Null-Check, um zu verhindern, dass unsere App abstürzt, wenn eine ungültige Position übergeben wurde
         if (taskToToggle != null) {
             if (taskToToggle.isClosed()) {
                 taskToToggle.markAsOpen();
@@ -78,19 +129,32 @@ public class MainActivity extends AppCompatActivity {
                 taskToToggle.markAsClosed();
             }
         }
+        // Nach der Änderung an der Datenbasis wird der Adapter aktualisiert
         updateTasksInAdapter();
     }
 
+    /**
+     * Sortiert die aktuelle Liste der Aufgaben und übergibt diese im Anschluss an den Adapter
+     */
     private void updateTasksInAdapter() {
+        // Zum Sortieren verwenden wir die Java-Collections-Klasse.
+        // Das funktioniert, weil die Objekte in der ArrayList, bzw. deren Klassen, das Comparable-Interface implementieren
         Collections.sort(tasks);
         taskListAdapter.setTasks(tasks);
     }
 
+    /**
+     * Prüft, ob sich der übergebene String als Beschreibung einer neuen Aufgabe eignet und, falls passend,
+     * erstellt auf dessen Basis eine neue Aufgabe in der ArrayList (dadurch wird auch der Adapter
+     * aktualsiert). Im Anschluss wird das UI zurückgesetzt, um die EIngabe der nächsten Aufgabe zu erleichtern.
+     *
+     * @param input
+     */
     private void onUserInputClicked(String input) {
         if (input.length() > 0) {
             addTask(input);
-            taskDescriptionInput.setText("");
-            taskDescriptionInput.requestFocus();
+            taskDescriptionInput.setText(""); // Löscht den aktuellen Inhalt aus dem Eingabefeld
+            taskDescriptionInput.requestFocus(); // Versuchst, das Eingabefeld zu fokussieren, so dass direkt die nächste Aufabe eingeben werden kann
         }
     }
 }
--- app/src/main/java/de/ur/mi/android/demos/todo/ui/TaskListAdapter.java
+++ app/src/main/java/de/ur/mi/android/demos/todo/ui/TaskListAdapter.java
@@ -19,50 +19,114 @@ import java.util.concurrent.TimeUnit;
 import de.ur.mi.android.demos.todo.R;
 import de.ur.mi.android.demos.todo.tasks.Task;
 
+/**
+ * Dieser Adapter verbindet eine ArrayList von Task-Objekten mit einem ListView im UserInterface. Der
+ * Adapter stellt dem ListView die notwendigen Views zu Darstellung der einzelnen Aufgaben bereit. Dabei
+ * unterscheidet der Adpater zwischen offen und bereits erledigten Aufgaben und gibt je nach Zustand der
+ * Aufgabe andere Views an das ListView weiter. Dadurch werden die beiden "Typen" von Aufgaben im
+ * User Interface auf unterschiedliche Art und Weise dargestellt.
+ */
 public class TaskListAdapter extends ArrayAdapter<Task> {
 
+    /**
+     * Achtung: Dieser Adapter nutzt die gleiche ArrayListe, die in der MainActivity zur Verwaltung
+     * der Aufgaben verwendet wird. Das ist kein gutes Software Design, da nun zwei Stellen unserer Anwendung
+     * (Activity und Adapter) unabhängig von einander die selbe Datenstruktur (mit für unserer App wichtigen
+     * Inhalten) verändern können. Idealerweise würde der Adapter über eine Kopie der Liste verfügen,
+     * die bei Änderungen an der Datenbasis (in der Activity) immer mit einer aktualisierten Kopie überschrieben würde.
+     */
+
+    // Liste der Aufgaben, die aktuell vom Adapter verwaltet werden
     private ArrayList<Task> tasks;
 
     public TaskListAdapter(@NonNull Context context) {
+        // Aufruf des Super-Konstruktors für den Adapter mit Übergabe eines Standardlayoputs für die Einträge (wir werden später unsere eigenen Layouts verwenden)
         super(context, android.R.layout.simple_list_item_1);
     }
 
+    /**
+     * Aktualisierte die Liste der Tasks, die der Adapter verwaltet, in dem die entsprechende Instanzvariable überschriebn wird
+     *
+     * @param tasks Neue Liste mit Tasks für den Adapter
+     */
     public void setTasks(ArrayList<Task> tasks) {
         this.tasks = tasks;
+        // Wenn sich die Datenbasis im Adapter ändert, informiert dieser den angeschlossenen ListView über die Änderungen
+        // Dadurch wird automatisch der Prozess gestartet, in dem das ListView einen neuen Satz an Views zur Darstellung
+        // der jetzt aktualisierten Inhalten vom Adapter anfordert
         this.notifyDataSetChanged();
     }
 
+    /**
+     * Ausgelagerte Funktion zum Erstellen eines neuen Views auf Basis einer XML-Datei
+     *
+     * @param resource Verweis auf die XML-Datei, die als Basis für das neuen View verwendet werden soll
+     * @param parent   Elternelement im UI, in das der neue View eingefügt werden soll
+     * @return Das erstellte View
+     */
     private View inflateViewTask(int resource, ViewGroup parent) {
+        // Für das Erstellen des Views aus der XML-Datei ist ein Inflater notwendig, den wir über den "Context" der App beziehen können
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         return inflater.inflate(resource, parent, false);
     }
 
+    /**
+     * Wird von dem ListView aufgerufen, wenn ein neuer View zur Darstellung im UI benötigt wird
+     *
+     * @param position    Die Position des ListViews, für die ein View mit Daten benmötigt wird
+     * @param convertView Ein möglicherweise schon vorhandener View, der im Vorfeld für die Darstellung dieser Listenposition verwendet wurde
+     * @param parent      Das Elternelement im UI, zu dem der neue View hinzugefügt werden kann, z.B. das ListView selber
+     * @return
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Wir identifzieren über den Positionsparameter, für welchen Datensatz wird einen View erzeugen sollen
         Task task = tasks.get(position);
         View viewForTask = convertView;
+        // Wenn es noch keinen View für diese Position gibt, erstellen wir einen neuen auf Basis der XML-Datei für eine "offene" Aufgabe
         if (viewForTask == null) {
             viewForTask = inflateViewTask(R.layout.task_list_item, parent);
         }
+        // Wenn es eine Aufgabe gibt, die angezeigt werden soll ...
         if (task != null) {
+            // ... prüfen wir, ob der aktuelle Typ von View passt, um den aktuellen Zustand der Aufgabe darzustellen
             if (task.isClosed() && viewForTask.getId() == R.id.list_item_default) {
+                // ... falls nicht, erstellen wir einen passenden View
                 viewForTask = inflateViewTask(R.layout.task_list_item_done, parent);
             } else if (!task.isClosed() && viewForTask.getId() == R.id.list_item_done) {
+                // ... falls nicht, erstellen wir einen passenden View
                 viewForTask = inflateViewTask(R.layout.task_list_item, parent);
             }
+            // Wenn wir sicher sind, dass Zustand der Aufgabe und Typ von View "überein stimmten", schreiben wir die eigentlichen Eigenschaften (Beschreibung und Datum) der Aufgabe in den View
             bindTaskToView(task, viewForTask);
         }
         return viewForTask;
     }
 
+    /**
+     * Überträgt die Eigenschaften der übergebenen Aufgabe in den View
+     *
+     * @param task Die Aufgabem deren Inhalte im View eingetragen werden sollen
+     * @param view Ein View zur Darstellung der Aufgabe (muss auf Basis der XML-Dateien task_list_item bzw. task_list_item_done erstellt worden sein)
+     */
     private void bindTaskToView(Task task, View view) {
+        // Referenzieren der einzelnen TextViews im übergebenen View
         TextView taskDescription = view.findViewById(R.id.list_item_description);
         TextView taskCreationDate = view.findViewById(R.id.list_item_creationDate);
+        // Auslesen der Task-Eigenschaften und übertragen in die jeweiligen TextViews
         taskDescription.setText(task.getDescription());
         taskCreationDate.setText(getFormattedDateForUI(task.getCreationDate()));
     }
 
+    /**
+     * Wandelt das übergeben Datum in einen sauber formatiertern String zur Darstellung im UI um.
+     * Datumsangaben werden in der Regel im Format Tag. Monat zurückgegeben. Liegt das übergebene Datum
+     * weniger als 24h zurück, wird statt dessen das Format  Stunden:Minuten verwendet.
+     *
+     * @param date Datum, das formatiert werden soll
+     * @return Formatierte Stringrepräsentation des übergebenen Datums
+     */
     private String getFormattedDateForUI(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault());
         Date now = new Date();
@@ -74,6 +138,11 @@ public class TaskListAdapter extends ArrayAdapter<Task> {
         return sdf.format(date);
     }
 
+    /**
+     * Wird vom ListView aufgerufen, um zu erfahren, wie viele Datensätze im Adapter verwaltet werden
+     *
+     * @return Anazhl der Einträge/Datensätze im Adapter (hier: Anzahl der Tasks in der ArrayList)
+     */
     @Override
     public int getCount() {
         if (tasks == null) {
