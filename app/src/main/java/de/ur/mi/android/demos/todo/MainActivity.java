1a3bd6817ee2d6f8f7d2a554b30f6a690755bda1
ragor114 <69634980+ragor114@users.noreply.github.com>
2022-05-10 16:40:53 +0200

    updated gradle, modernized design

--- app/src/main/java/de/ur/mi/android/demos/todo/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/demos/todo/MainActivity.java
@@ -1,10 +1,11 @@
 package de.ur.mi.android.demos.todo;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/de/ur/mi/android/demos/todo/tasks/Task.java
+++ app/src/main/java/de/ur/mi/android/demos/todo/tasks/Task.java
@@ -5,15 +5,15 @@ import java.util.UUID;
 
 /**
  * Repräsentiert eine Aufgabe auf der ToDo-liste.
- * <p>
+ *
  * Aufgaben verfügen über eine eindeutige ID, eine textuelle Beschreibung und ein Erstellungs-
  * datum. Diese Eigenschaften sind nach dem Erzeugen einer Aufgabe nicht veränderbar. Zusätzlich
  * wird für jede Aufgabe festgehalten, ob diese offen oder erledigt ist. Der Zustand kann über
  * öffentliche Methoden geändert werden.
- * <p>
+ *
  * Kopien
  * Über die copy-Methode können tiefe (deep) Kopien eines Task-Objekts erstellt werden.
- * <p>
+ *
  * Sortierung
  * Die Task-Klasse implementiert das Comparable-Interface das die Sortierung von Task-Objekten
  * ermöglicht. Offene Aufgaben werden vor geschlossenen Aufgaben einsportiert. Aufgaben mit
